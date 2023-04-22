c3b3c42436336d5725f601b20477a29fd577b682
Bryan Tony <ciprianot.bryan@gmail.com>
2023-04-22 04:03:13 -0500

    [QA] Mysql-Reactive' properties added

--- src/main/java/com/scotia/student/storage/repository/StudentRepository.java
+++ src/main/java/com/scotia/student/storage/repository/StudentRepository.java
@@ -1,11 +1,11 @@
 package com.scotia.student.storage.repository;
 
 import com.scotia.student.storage.repository.entity.*;
-import org.springframework.data.repository.reactive.*;
+import org.springframework.data.r2dbc.repository.*;
 import org.springframework.stereotype.*;
 import reactor.core.publisher.*;
 
 @Repository
-public interface StudentRepository extends ReactiveCrudRepository<Student, Integer> {
+public interface StudentRepository extends R2dbcRepository<Student, Integer> {
     Flux<Student> findAllByStatusIsTrue();
 }
